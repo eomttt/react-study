6dbf17aef47e59fe135f71317775ba92bc0c2381
hyuntaeeom <hyuntaeeom@hyuntaeeomui-MacBookPro.local>
2019-07-04 16:50:13 +0900

    Add comment out in responseCheck

--- response-check/ResponseCheck.jsx
+++ response-check/ResponseCheck.jsx
@@ -15,6 +15,7 @@ class ResponseCheck extends Component {
     const { state, message, result } = this.state;
 
     if (state === 'waiting') {
+      // waiting 일 때 클릭시 타이머를 돌려서 랜덤하게 now 화면으로 변하게 해준다.
       this.clickTimer = setTimeout(() => {
         this.setState({
           state: 'now',
@@ -29,6 +30,7 @@ class ResponseCheck extends Component {
         message: 'Click to when screen is green.'
       });
     } else if (state === 'ready') {
+      // ready 일 때 클릭시 타이머를 초기화 해주고 경고 메세지를 보여준다.
       clearTimeout(this.clickTimer);
 
       this.setState({
@@ -36,6 +38,7 @@ class ResponseCheck extends Component {
         message: 'So fast, Please click when screen is green.'
       });
     } else if (state === 'now') {
+      // now 일 때 클릭시 클릭 한 시간을 저장하고 그 차이를 저장하여 반응속도 시간을 구한다.
       this.endTime = new Date();
       this.setState((prevState) => {
         return {
